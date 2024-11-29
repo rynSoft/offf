cac6e388b67398d5562b3b3ba26f366cc4665267
RynSoft <hakannyildirim@gmail.com>
2024-11-29 12:04:12 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridResults = new System.Windows.Forms.DataGridView();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.txtQuery = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.paremeterBasvuruNo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tokenn = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridResults)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridResults
+            // 
+            this.dataGridResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridResults.Location = new System.Drawing.Point(198, 298);
+            this.dataGridResults.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.dataGridResults.Name = "dataGridResults";
+            this.dataGridResults.RowHeadersWidth = 72;
+            this.dataGridResults.Size = new System.Drawing.Size(1548, 364);
+            this.dataGridResults.TabIndex = 9;
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(1765, 444);
+            this.btnExecute.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(161, 95);
+            this.btnExecute.TabIndex = 8;
+            this.btnExecute.Text = "Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // txtQuery
+            // 
+            this.txtQuery.Location = new System.Drawing.Point(198, 40);
+            this.txtQuery.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.txtQuery.Multiline = true;
+            this.txtQuery.Name = "txtQuery";
+            this.txtQuery.Size = new System.Drawing.Size(1548, 144);
+            this.txtQuery.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 252);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 25);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Başvuru No :";
+            // 
+            // paremeterBasvuruNo
+            // 
+            this.paremeterBasvuruNo.Location = new System.Drawing.Point(198, 191);
+            this.paremeterBasvuruNo.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.paremeterBasvuruNo.Multiline = true;
+            this.paremeterBasvuruNo.Name = "paremeterBasvuruNo";
+            this.paremeterBasvuruNo.Size = new System.Drawing.Size(1548, 84);
+            this.paremeterBasvuruNo.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 101);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 25);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Cümlecik :";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Location = new System.Drawing.Point(1765, 40);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 92);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Case 1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tokenn
+            // 
+            this.tokenn.Location = new System.Drawing.Point(198, 713);
+            this.tokenn.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.tokenn.Multiline = true;
+            this.tokenn.Name = "tokenn";
+            this.tokenn.Size = new System.Drawing.Size(1548, 84);
+            this.tokenn.TabIndex = 16;
+            this.tokenn.Text = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJfMjA1MzA5XyIsImF1dGgiOiJST0xFX1VTRVIiLCJleHAiOjE3" +
+    "MzI5MjUyNzh9.lRj5sdq69j25BC5GAuf1f8rYD0WiwlSsmf5569lO4B8NNneJXI6CtzRXstnyJOk0q2i" +
+    "B2gsP5ve9Th-W6gthTQ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(87, 742);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 25);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Token : ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1799, 713);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(161, 88);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "createPdfFile";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.IndianRed;
+            this.button3.Location = new System.Drawing.Point(1765, 160);
+            this.button3.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(161, 92);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "Case 2";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Chartreuse;
+            this.button4.Location = new System.Drawing.Point(1765, 264);
+            this.button4.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(161, 92);
+            this.button4.TabIndex = 20;
+            this.button4.Text = "Case 3";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(37, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(201, 29);
+            this.textBox1.TabIndex = 21;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(305, 48);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(201, 29);
+            this.textBox2.TabIndex = 22;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(507, 811);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(894, 135);
+            this.panel1.TabIndex = 23;
+            this.panel1.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(90, 13);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 25);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Token : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(379, 13);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 25);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "Token : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1973, 1068);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tokenn);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.paremeterBasvuruNo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridResults);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.txtQuery);
+            this.Controls.Add(this.label2);
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Form1";
+            this.Text = "Oracle Database Query";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridResults)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridResults;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.TextBox txtQuery;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox paremeterBasvuruNo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tokenn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+using NLog;
+using Oracle.ManagedDataAccess.Client;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        private static readonly HttpClient client = new HttpClient();
+        // Oracle Database Connection String
+        private const string connectionString =
+            "User Id=SBS;Password=San2934BS+Prod;" +
+            "Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=soradb-scan.tobb.org.tr)(PORT=1521))" +
+            "(CONNECT_DATA=(SERVICE_NAME=TOBB)))";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Optional: Add any initialization code here
+        }
+
+        #region Case 1 -  Eksper veya Raportor E-imzasını atarsaki durumu
+        //// Case 1 -  Eksper veya Raportor E-imzasını atarsaki durumu
+
+        ////Begin transaction  
+
+        //// 1 - Efeden Basvuru No'ları alıcaz;  +++++++
+
+        //// 2 - FIR_BASV gelen basvuru nolardan  "Id" yi al;  +++++
+
+        //// 3 - 2' den aldıgın "ID" ile FIR_BASV_ISL "FIRMA_BASVURU_ID" yi ilişkilendir ve BasvuruDurumuId = 16 ve 24 olan kayıtları sil.  ++++++
+
+        //// 4 - Entpointi çağır.
+
+        //// 5 - FIR_BASV_IMZA_JOB da bulunan Role alanındaki "ODA_EKSPER" "Job_Statusunu" pending yapacaz . 
+
+        //// 6 - FIR_BASV_IMZA_JOB da bulunan Role alanındaki "ODA_YONETICI" Job_Statusunu "not_assigined" yapacaz.
+
+        //// 7 - FIR_BASV_IMZA_JOB da bulunan Role alanındaki "TOBB_YONETICI" Job_Statusunu "not_assigined" yapacaz.
+        #endregion
+        private void Case_1()
+        {
+            try
+            {
+                using (OracleConnection connection = new OracleConnection(connectionString))
+                {
+
+                    connection.Open();
+
+                    OracleCommand command = new OracleCommand("SELECT ID,KAPASITE_RAPORU_ID FROM SBS.FIR_BASV WHERE BASVURU_NO IN (" + paremeterBasvuruNo.Text + ")", connection);
+                    OracleDataReader rdr = command.ExecuteReader();
+
+                    while (rdr.Read())
+                    {
+                        //MessageBox.Show($"FIR_BASV Data - Id : {rdr.GetString(0)} KapasiteRaporuId : {rdr.GetString(1)}");
+                        Logger.Info($"FIR_BASV Data - Id : {rdr.GetString(0)} KapasiteRaporuId : {rdr.GetString(1)}");
+
+                        // Begin transaction
+                        using (OracleTransaction transaction = connection.BeginTransaction())
+                        {
+                            try
+                            {
+                                // Execute first command
+                                using (OracleCommand commandbasvIsl = new OracleCommand("DELETE FROM SBS.FIR_BASV_ISL WHERE FIRMA_BASVURU_ID = " + rdr.GetString(0) + " AND BASVURU_DURUM_ID IN ( 16 , 24 ) ", connection))
+                                {
+                                    commandbasvIsl.Transaction = transaction;
+                                    commandbasvIsl.ExecuteNonQuery();
+                                }
+                                MessageBox.Show($"FIR_BASV Data - Id : {rdr.GetString(0)} KapasiteRaporuId : {rdr.GetString(1)}");
+
+                                // Execute second command
+                                using (OracleCommand commandOdaEksper = new OracleCommand("UPDATE FIR_BASV_IMZA_JOB SET JOB_STATUS = 'PENDING' WHERE  FIRMA_BASVURU_ID = " + rdr.GetString(0) + " AND ROL = 'ODA_EKSPER'", connection))
+                                {
+                                    commandOdaEksper.Transaction = transaction;
+                                    commandOdaEksper.ExecuteNonQuery();
+                                }
+
+                                // Execute third command
+                                using (OracleCommand commandOdaYonetici = new OracleCommand("UPDATE FIR_BASV_IMZA_JOB SET JOB_STATUS = 'NOT_ASSIGNED' WHERE  FIRMA_BASVURU_ID = " + rdr.GetString(0) + " AND ROL = 'ODA_YONETICI'", connection))
+                                {
+                                    commandOdaYonetici.Transaction = transaction;
+                                    commandOdaYonetici.ExecuteNonQuery();
+                                }
+
+                                // Execute fourth command
+                                using (OracleCommand commandTobbYonetici = new OracleCommand("UPDATE FIR_BASV_IMZA_JOB SET JOB_STATUS = 'NOT_ASSIGNED' WHERE  FIRMA_BASVURU_ID = " + rdr.GetString(0) + " AND ROL='TOBB_YONETICI'", connection))
+                                {
+                                    commandTobbYonetici.Transaction = transaction;
+                                    commandTobbYonetici.ExecuteNonQuery();
+                                }
+
+                                bool isSuccess = long.TryParse(rdr.GetString(0), out long Sayi);
+                                readyrequstData(Sayi, rdr.GetString(1)).GetAwaiter().GetResult();
+
+                                // Commit the transaction
+                                transaction.Commit();
+                                MessageBox.Show("Transaction committed successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                            catch (Exception ex)
+                            {
+                                // Rollback the transaction in case of an error
+                                transaction.Rollback();
+                                Logger.Error(ex, $"Transaction rolled back due to error: {ex.Message}");
+                            }
+                        }
+                    }
+                 
+                    rdr.Dispose();
+                    command.Dispose();
+
+                    Console.WriteLine("Both records are written to database.");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async Task readyrequstData(long firmaBasvuruIds, string kapasiteNos)
+        {
+            try
+            {
+                string token = "Bearer " + tokenn.Text + ""; // Bearer Token buraya gelecek
+                var requestData = new
+                {
+                    rapor = 26,
+                    raporFormat = 0,
+                    firmaBasvuruId = firmaBasvuruIds, //917832,
+                    kapasiteNo = kapasiteNos //"1005154"
+                };
+               
+                string response = await SendPostRequest("https://sanayi.org.tr/api/prepare-capacity-report", requestData, token);
+               
+                if (string.IsNullOrEmpty(response))
+                   Logger.Info("", $"readyrequstData : ----------  response : {response} firmaBasvuruIds : {firmaBasvuruIds} kapasiteNos : {kapasiteNos} hata olustu...");
+                else
+                    Logger.Info("", $"readyrequstData : +++++++++ response : {response} firmaBasvuruIds : {firmaBasvuruIds} kapasiteNos : {kapasiteNos} tamamlandı...");
+
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("readyrequstData Unexpected Error : response ",  ex.Message);
+            }
+        }
+
+        private async Task<string> SendPostRequest(string url, object data, string token)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    // Token'ı Authorization Header'a ekle
+                    client.DefaultRequestHeaders.Add("Authorization", token);
+
+                    //// Gönderilecek JSON verisini oluştur
+                    string jsonData = System.Text.Json.JsonSerializer.Serialize(data);
+                    var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+                    // POST isteğini gönder ve yanıtı al
+                    HttpResponseMessage response =  client.PostAsync(url, content).GetAwaiter().GetResult();
+
+                    Logger.Info("SendPostRequest: response ", response);
+                    //MessageBox.Show("response : " + response);
+                    // Başarısız durum kodlarında hata fırlat
+                    response.EnsureSuccessStatusCode();
+
+                    // Yanıtı string olarak oku
+                    return await response.Content.ReadAsStringAsync();
+                }
+                catch (Exception  ex)
+                {
+                    Logger.Error("SendPostRequest Unexpected Error : response ", ex.Message);
+                    //MessageBox.Show("SendPostRequest ex : " + ex.Message);
+                }
+             
+            }  
+            return "";
+        }
+
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            string query = txtQuery.Text;
+
+            if (string.IsNullOrWhiteSpace(query))
+            {
+                MessageBox.Show("Please enter a valid SQL query.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            query = "SELECT * FROM SBS.FIR_BASV WHERE BASVURU_NO IN (" + paremeterBasvuruNo.Text + ")";
+
+            try
+            {
+                using (OracleConnection connection = new OracleConnection(connectionString))
+                {
+                    connection.Open();
+                    OracleCommand command = new OracleCommand(query, connection);
+
+                    OracleDataAdapter adapter = new OracleDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    dataGridResults.DataSource = dataTable;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Case_1();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            readyrequstData(914069, "1000813").GetAwaiter().GetResult();
+        }
+
+
+        #region Case 1 -  Eksper veya Raportor E-imzasını atarsaki durumu
+        //// Case 1 -  Eksper veya Raportor E-imzasını atarsaki durumu
+
+        ////Begin transaction  
+
+        //// 1 - Efeden Basvuru No'ları alıcaz;  +++++++
+
+        //// 2 - FIR_BASV gelen basvuru nolardan  "Id" yi al;  +++++
+
+        //// 3 - Entpointi çağır.
+
+        #endregion
+        private void Case_2()
+        {
+            try
+            {
+                using (OracleConnection connection = new OracleConnection(connectionString))
+                {
+                    connection.Open();
+
+                    OracleCommand command = new OracleCommand("SELECT ID,KAPASITE_RAPORU_ID FROM SBS.FIR_BASV WHERE BASVURU_NO IN (" + paremeterBasvuruNo.Text + ")", connection);
+                    OracleDataReader rdr = command.ExecuteReader();
+
+                    while (rdr.Read())
+                    {
+                        bool isSuccess = long.TryParse(rdr.GetString(0), out long Sayi);
+                        readyrequstData(Sayi, rdr.GetString(1)).GetAwaiter().GetResult();
+                    }
+
+                    rdr.Dispose();
+                    command.Dispose();
+
+                    Console.WriteLine("Both records are written to database.");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Case_2();
+        }
+
+        private void Case_3()
+        {
+            try
+            {
+                using (OracleConnection connection = new OracleConnection(connectionString))
+                {
+                    connection.Open();
+
+                    OracleCommand command = new OracleCommand("SELECT ID,KAPASITE_RAPORU_ID FROM SBS.FIR_BASV WHERE BASVURU_NO IN (" + paremeterBasvuruNo.Text + ")", connection);
+                    OracleDataReader rdr = command.ExecuteReader();
+
+                    while (rdr.Read())
+                    {
+                        bool isSuccess = long.TryParse(rdr.GetString(0), out long Sayi);
+                        readyrequstData(Sayi, rdr.GetString(1)).GetAwaiter().GetResult();
+                    }
+
+                    rdr.Dispose();
+                    command.Dispose();
+
+                    Console.WriteLine("Both records are written to database.");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Case_3();
+        }
+    }
+}
